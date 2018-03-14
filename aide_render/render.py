8564b9f5799f401588fab9b478497b2736901657
ethan92429 <ethan.keller@gmail.com>
2018-03-14 14:01:23 -0400

    Added assert_inputs functionality

--- aide_render/render.py
+++ aide_render/render.py
@@ -7,7 +7,6 @@ import os
 import sys
 import re
 from aide_design.play import *
-import pint
 
 """Implement a "print for Fusion" function that takes a pint value and prints it as a
 string-representable Fusion unit input.
@@ -57,10 +56,13 @@ def render_constants(stream):
 
 
 def render(stream, d=None):
-    """
-    Render the template with d variables (a dict) and return the rendered file
+    """Render the template with d variables (a dict) and return the rendered file
     using the aide_render environment.
     This method can be called from within templates.
+
+    Examples
+    --------
+
     >>> from aide_design.play import *
     >>> import os
     >>> d = {"u":u}
@@ -89,6 +91,113 @@ def render(stream, d=None):
         env.globals.update(variables)
     template = env.get_template("template")
     return yaml.load(template.render())
+
+
+def source_from_path(file_path: str) -> str:
+    """
+
+    Parameters
+    ----------
+    file_path: Absolute path to the file to read.
+
+    Returns
+    -------
+    str
+        The contents of the file.
+
+    """
+    with open(file_path) as f:
+        return f.read()
+
+
+def assert_inputs(variables:dict, types_dict:dict, strict=True, silent=False):
+    """Check variables against their expected type. Also can check more complex types, such as whether the expected
+    and actual dimensionality of pint units are equivalent.
+
+    Parameters
+    ----------
+    variables : dict
+        Contain variable_name : variable_object key-value pairs.
+    types_dict : dict
+        A dictionary containing variable:type key value pairs.
+    strict :obj: bool, optional
+        If true, the function ensures all the variables in types_dict are present in variables.
+    silent :obj: bool, optional
+        If true, the function only returns a boolean rather than throwing an error.
+
+    Returns
+    -------
+    bool
+        True if the variables dict passes the assert_inputs check as described.
+
+    Raises
+    ------
+    ValueError
+        If silent is turned to false and the inputs do not pass the assert_inputs test
+
+    Examples
+    --------
+
+    Standard usage showing a passing collection of parameters. This passes both the non-strict and strict options.
+
+    >>> variables = {"a" : 1, "b" : 1.0, "c" : "string"}
+    >>> types_dict = {"a" : int, "b" : float, "c" : str}
+    >>> assert_inputs(variables,types_dict)
+    True
+
+    Wrong types error thrown:
+
+    >>> types_dict = {"a" : str, "b" : str, "c" : str}
+    >>> assert_inputs(variables,types_dict)
+    Traceback (most recent call last):
+    ValueError: Wrong var types: {'a': "Actual type: <class 'int'> Intended type: <class 'str'>", 'b': "Actual type: <class 'float'> Intended type: <class 'str'>"} missing: []
+
+    Not enough variables are present:
+    >>> assert_inputs({"a":1}, {"a":int, "b": int})
+    Traceback (most recent call last):
+    ValueError: Wrong var types: {} missing: ['b']
+
+    Types with pint units:
+
+    >>> from aide_design.play import *
+    >>> assert_inputs({"length" : 1*u.meter},{"length" : u.mile})
+    True
+    >>> assert_inputs({"length" : 1*u.meter**2},{"length" : u.mile})
+    Traceback (most recent call last):
+    ValueError: Wrong var types: {'length': 'Actual dimensionality: [length] ** 2 Intended dimensionality: [length]'} missing: []
+
+    """
+    check = True
+    # Store the intended types
+    type_error_dicionary = {}
+    # Store the missing variables
+    missing = []
+
+    for name, t in types_dict.items():
+        try:
+            var = variables[name]
+
+            # check if this is a pint variable and has compatible dimensionality.
+            if isinstance(var, u.Quantity):
+                if not var.dimensionality == t.dimensionality:
+                    type_error_dicionary[name] = "Actual dimensionality: {} Intended dimensionality: {}".format(
+                        var.dimensionality, t.dimensionality)
+
+            # check if types are compatible
+            elif not isinstance(var, t):
+                type_error_dicionary[name] = "Actual type: {} Intended type: {}".format(type(var), t)
+
+        # If the variable is missing
+        except KeyError:
+            missing.append(name)
+
+    check = not type_error_dicionary
+    if strict and check:
+        check = not missing
+
+    if not silent and not check:
+        raise ValueError("Wrong var types: {} missing: {}".format(type_error_dicionary, missing))
+    return check
 
 
 def create_aide_environment(template_fp):
--- tests/test_render.py
+++ tests/test_render.py
@@ -4,19 +4,10 @@ aide_render tests.
 
 import pytest
 from aide_design.play import *
-from aide_render.render import render, render_constants
+from aide_render.render import render, render_constants, assert_inputs, source_from_path
 from aide_render import yaml
 import os
-import sys
 
-
-# s = """this:
-#       - a
-#       - test
-#       - of
-#     the strip_jinja function: [ {{did it work?}} ]
-#     {% how about now? %}
-#     """
 
 def test_yaml_units():
     """
@@ -25,13 +16,13 @@ def test_yaml_units():
     s = "!u 1 meter"
     assert yaml.load(s) == 1*u.meter
 
+
 def test_render_constants():
     """
     Test some simple renderings from tests/test_templates/simple
     """
     file_path = os.path.abspath('tests/test_templates/simple/test_render_constants.yaml')
-    d = {"u": u}
-    assert render(open(file_path, 'r'), d) == {'implicitly_defined_quantity': 45*u.meter, 'that constant as a Jinja variable': 45*u.meter}
+    assert render_constants(open(file_path, 'r')) == {'implicitly_defined_quantity': 45*u.meter, 'that constant as a Jinja variable': None}
 
 
 def test_render_recursive():
@@ -43,3 +34,15 @@ def test_render_recursive():
     d = {"u": u, "os": os, "render": render, "open": open}
     print(render(open(file_path, 'r'), d))
     assert render(open(file_path, 'r'), d) == yaml.load(open(output_file_path, 'r'))
+
+
+def test_assert_inputs():
+    """
+    Test assert_inputs asserts the correct types without throwing an error, and test for when an error is thrown.
+    """
+    assert assert_inputs({"a": 1, "b": 2.0, "c": "3", "d": 4*u.meter}, {"a": int, "b": float, "c": str, "d": u.mile})
+
+
+def test_source_from_path():
+    file_path = os.path.abspath("tests/test_source_from_path.txt")
+    assert source_from_path(file_path) == "This is some source text."
